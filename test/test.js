4d90562ce510d05d385a7dd7fa150c226d8a88db
yehiyam <yehiyam@gmail.com>
2018-03-26 13:12:47 +0300

    add get by prefix for discovery

--- test/test.js
+++ test/test.js
@@ -54,54 +54,102 @@ describe('etcd', () => {
             const instanceId = `register-test-${uuidv4()}`;
             const discovery = new Discovery();
             await discovery.init({
-                serviceName:'test-service',
+                serviceName: 'test-service',
                 instanceId,
-                client:etcd._client
+                client: etcd._client
             })
             await discovery.register({});
-            let expected = {foo:'bar'};
+            let expected = { foo: 'bar' };
             await discovery.updateRegisteredData(expected)
             const path = `/discovery/test-service/${instanceId}`;
             let actual = await etcd._client.get(path);
             expect(JSON.parse(actual[path])).to.eql(expected);
 
-            expected = {foofoo:'barbar'};
+            expected = { foofoo: 'barbar' };
             await discovery.updateRegisteredData(expected)
             actual = await etcd._client.get(path);
             expect(JSON.parse(actual[path])).to.eql(expected);
-            
+
         }).timeout(5000);
         it('should get data from discovery with serviceName', async () => {
             const instanceId = `register-test-${uuidv4()}`;
             const discovery = new Discovery();
             await discovery.init({
-                serviceName:'test-service',
+                serviceName: 'test-service',
                 instanceId,
-                client:etcd._client
+                client: etcd._client
             })
             await discovery.register({});
-            let expected = {foo:'bar'};
+            let expected = { foo: 'bar' };
             await discovery.updateRegisteredData(expected)
             let actual = await discovery.get({
-                serviceName:'test-service',
-                prefix:'test-service',
+                serviceName: 'test-service',
+                prefix: 'test-service',
                 instanceId
             });
             expect(actual).to.eql(expected);
+        });
+        it('should get prefix data from discovery with serviceName', async () => {
+            const instanceId = `register-test-${uuidv4()}`;
+            const discovery = new Discovery();
+            await discovery.init({
+                serviceName: 'test-service',
+                instanceId,
+                client: etcd._client
+            })
+            await discovery.register({});
+            let expected = { foo: 'bar' };
+            await discovery.updateRegisteredData(expected)
+
+            let actual = await discovery.get({
+                serviceName: 'test-service'
+            });
+            expect(actual).deep.include({ [`/discovery/test-service/${instanceId}`]: expected })
+        });
+        it('should get prefix data from discovery with serviceName - multiple results', async () => {
+            const instanceId = `register-test-${uuidv4()}`;
+            const discovery = new Discovery();
+            await discovery.init({
+                serviceName: 'test-service',
+                instanceId,
+                client: etcd._client
+            })
+            await discovery.register({});
+            let expected = { foo: 'bar' };
+            await discovery.updateRegisteredData(expected)
+
+            const etcd2 = new Etcd();
+            await etcd2.init({ etcd: { host: 'localhost', port: 4001 }, serviceName: SERVICE_NAME });
+            const instanceId2 = `register-test-${uuidv4()}`;
+            
+            const discovery2 = new Discovery();
+            await discovery2.init({
+                serviceName: 'test-service',
+                instanceId: instanceId2,
+                client: etcd2._client
+            })
+            await discovery2.register({});
+            await discovery2.updateRegisteredData({ foo: 'baz' })
+
+            let actual = await discovery.get({
+                serviceName: 'test-service'
+            });
+            expect(actual).deep.include({ [`/discovery/test-service/${instanceId}`]: expected })
+            expect(actual).deep.include({ [`/discovery/test-service/${instanceId2}`]: { foo: 'baz' } })
         });
         it('should get data from discovery without serviceName', async () => {
             const instanceId = `register-test-${uuidv4()}`;
             const discovery = new Discovery();
             await discovery.init({
-                serviceName:'test-service',
+                serviceName: 'test-service',
                 instanceId,
-                client:etcd._client
+                client: etcd._client
             })
             await discovery.register({});
-            let expected = {foo:'bar'};
+            let expected = { foo: 'bar' };
             await discovery.updateRegisteredData(expected)
             let actual = await discovery.get({
-                prefix:'test-service',
+                prefix: 'test-service',
                 instanceId
             });
             expect(actual).to.eql(expected);
@@ -110,16 +158,16 @@ describe('etcd', () => {
             const instanceId = `register-test-${uuidv4()}`;
             const discovery = new Discovery();
             await discovery.init({
-                serviceName:'test-service',
+                serviceName: 'test-service',
                 instanceId,
-                client:etcd._client
+                client: etcd._client
             })
             await discovery.register({});
-            let expected = {foo:'bar'};
+            let expected = { foo: 'bar' };
             await discovery.updateRegisteredData(expected)
             let actual = await discovery.get({
-                serviceName:'test-service-wrong',
-                prefix:'test-service',
+                serviceName: 'test-service-wrong',
+                prefix: 'test-service',
                 instanceId
             });
             expect(actual).to.be.null;
