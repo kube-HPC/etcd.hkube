cd5053c70f0c4bf573e711d8ec2b84ac3978cb03
Nassi Harel <nassi9@gmail.com>
2019-01-09 15:36:26 +0200

    add drivers

--- client.js
+++ client.js
@@ -11,6 +11,7 @@ const Pipelines = require('./lib/pipelines/pipelines');
 const Execution = require('./lib/execution/execution');
 const RunningPipelines = require('./lib/execution/running-pipelines');
 const Workers = require('./lib/workers/workers');
+const Drivers = require('./lib/drivers/drivers');
 const { PipelineDriverQueue, PipelineDriverRequirements, PipelineDriverTemplatesStore } = require('./lib/pipeline-driver');
 const AlgorithmDebug = require('./lib/debug/debug');
 const { AlgorithmQueue, ResourceRequirements, TemplatesStore } = require('./lib/algorithms/index');
@@ -31,6 +32,7 @@ class Client {
         this.execution = new Execution();
         this.runningPipelines = new RunningPipelines();
         this.workers = new Workers();
+        this.drivers = new Drivers();
         this.algorithms = {};
         this.algorithms.algorithmQueue = new AlgorithmQueue();
         this.algorithms.resourceRequirements = new ResourceRequirements();
@@ -72,6 +74,7 @@ class Client {
             this.tasks.init(data);
             this.jobState.init(data);
             this.workers.init(data);
+            this.drivers.init(data);
             this.pipelines.init(data);
             this.execution.init(data);
             this.runningPipelines.init(data);
